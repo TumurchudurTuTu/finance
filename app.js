1a63159074c167216e876a9ddb55937ddecc84c0
tumurchudur <ttankerman@gmail.com>
2021-11-17 18:42:29 +0800

    Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Delgetstei ajillah controller 
+var uiController = (function(){
+  
+})();
+// Sanhuutei ajillah controller 
+var financeController = (function(){
+    
+})();
+// Programmiin holbogch controller
+var appController = (function(uiController, financeController){
+})(uiController, financeController);
