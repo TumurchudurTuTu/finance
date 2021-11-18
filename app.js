373cba56b774d1ea1b4d1875c0ef663b9c7651ed
tumurchudur <ttankerman@gmail.com>
2021-11-18 15:08:11 +0800

    Event listeneruudiig uusgej ogloo

--- app.js
+++ app.js
@@ -8,4 +8,28 @@ var financeController = (function(){
 })();
 // Programmiin holbogch controller
 var appController = (function(uiController, financeController){
+var ctrlAddItem = function(){
+    // 1. Oruulah ogogdliig delgetsees olj avna.
+    console.log('hha');
+    // 2. Olj avsan ogogdluudee sanhuugiin controllert damjuulj tend hadgalana.
+
+    // 3. Olj avsan ogogdluudee web deeree tohiroh hesegt ni gargana.
+
+    // 4. Tusviig tootsoolno.
+
+    // 5. Etssiin uldegdel, tootsoog delgetsend gargana.
+}
+document.querySelector('.add__btn').addEventListener('click', function(){
+    ctrlAddItem();
+})
+document.addEventListener('keypress', function(event){
+    // console.log(event);
+    if (event.key === 'Enter' || event.which === 13){
+        ctrlAddItem();
+    }
+    
+    
+})
+
+
 })(uiController, financeController);
