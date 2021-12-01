0621cbd10701a6ecfa4eab818218e3db664a2689
TuTu <ttankerman@gmail.com>
2021-12-02 00:34:04 +0800

    aa

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
-// Delgetstei ajillah controller 
-var uiController = (function(){
-  
+// Delgetstei ajillah controller
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+    getDomStrings: function () {
+      return DOMstrings;
+    },
+  };
 })();
-// Sanhuutei ajillah controller 
-var financeController = (function(){
-    
-})();
+// Sanhuutei ajillah controller
+var financeController = (function () {})();
 // Programmiin holbogch controller
-var appController = (function(uiController, financeController){
-var ctrlAddItem = function(){
+var appController = (function (uiController, financeController) {
+  var ctrlAddItem = function () {
     // 1. Oruulah ogogdliig delgetsees olj avna.
-    console.log('hha');
+    console.log(uiController.getInput());
     // 2. Olj avsan ogogdluudee sanhuugiin controllert damjuulj tend hadgalana.
 
     // 3. Olj avsan ogogdluudee web deeree tohiroh hesegt ni gargana.
@@ -18,18 +34,27 @@ var ctrlAddItem = function(){
     // 4. Tusviig tootsoolno.
 
     // 5. Etssiin uldegdel, tootsoog delgetsend gargana.
-}
-document.querySelector('.add__btn').addEventListener('click', function(){
-    ctrlAddItem();
-})
-document.addEventListener('keypress', function(event){
-    // console.log(event);
-    if (event.key === 'Enter' || event.which === 13){
+  };
+
+  var setupEventListeners = function () {
+    var DOM = uiController.getDomStrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
+      ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.key === "Enter" || event.which === 13) {
         ctrlAddItem();
-    }
-    
-    
-})
-
-
+      }
+    });
+  };
+  return {
+    init: function () {
+      console.log("Application started...");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
